c584229a0b9b658551d30ecfe9998cb140c2439e
PauloVitorPaiva <paulovitor9931@gmail.com>
2025-01-17 10:36:59 -0300

    ex008 a 012

---
+++ exercicio/ex010.js
@@ -0,0 +1,7 @@
+let pais = 'Eua'
+if (pais == 'Brazil') {
+    console.log('você é brasileiro')
+}
+else {
+    console.log('você é Estrangeiro')
+}
---
+++ exercicio/ex09.js
@@ -0,0 +1,6 @@
+let vel = 60.5
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade permitido. multado')
+}
+console.log('Dirija sempre usando o cinto de segurança')
